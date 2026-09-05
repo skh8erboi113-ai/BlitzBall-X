84d29e9ce03107cf8262e49d6746a5c152e630ed
Brenden <sk.h8er.boi113@gmail.com>
2026-09-04 17:53:04 -0700

    Update free_agency.gd

--- scripts/ui/free_agency.gd
+++ scripts/ui/free_agency.gd
@@ -7,12 +7,13 @@ extends Control
 @onready var budget_label: Label = $TopBar/BudgetLabel
 @onready var back_btn: Button = $TopBar/BackButton
 
-var scouting_mgr: ScoutingManager = null
-var current_team: BlitzballTeamData = null
-var selected_agent: BlitzballPlayerData = null
+var scouting_mgr: Node = null
+var current_team: Resource = null
+var selected_agent: Resource = null
 
 func _ready() -> void:
-	scouting_mgr = ScoutingManager.new()
+	var scouting_script = preload("res://scouting_manager.gd")
+	scouting_mgr = scouting_script.new()
 	add_child(scouting_mgr)
 
 	current_team = TeamDatabase.teams.get(GameManager.current_home_team_id)
@@ -24,18 +25,19 @@ func _ready() -> void:
 
 func _refresh_ui() -> void:
 	if current_team:
-		budget_label.text = "TEAM BUDGET: %d Gil" % current_team.budget
+		budget_label.text = "TEAM BUDGET: %d Gil" % current_team.get("budget")
 
 	agent_list.clear()
-	for i in range(scouting_mgr.free_agents.size()):
-		var a := scouting_mgr.free_agents[i]
-		var pos_str := BlitzballPlayerData.Position.keys()[a.position].substr(0, 2)
-		var item_str := "%-3s | %-16s | OVR: %2d | Cost: %4d Gil" % [
-			pos_str, a.get_full_name(), a.get_overall_rating(), a.salary
+	var list_agents: Array = scouting_mgr.get("free_agents")
+	for i in range(list_agents.size()):
+		var a = list_agents[i]
+		var pos_num: int = a.get("position")
+		var item_str := "FA | %-16s | OVR: %2d | Cost: %4d Gil" % [
+			a.get("first_name"), a.call("get_overall_rating"), a.get("salary")
 		]
 		agent_list.add_item(item_str)
 
-	if scouting_mgr.free_agents.size() > 0:
+	if list_agents.size() > 0:
 		agent_list.select(0)
 		_on_agent_selected(0)
 	else:
@@ -43,25 +45,25 @@ func _refresh_ui() -> void:
 		sign_btn.disabled = true
 
 func _on_agent_selected(index: int) -> void:
-	if index < 0 or index >= scouting_mgr.free_agents.size():
+	var list_agents: Array = scouting_mgr.get("free_agents")
+	if index < 0 or index >= list_agents.size():
 		return
 
-	selected_agent = scouting_mgr.free_agents[index]
-	var text := "[b][font_size=22]%s[/font_size][/b]\n" % selected_agent.get_full_name()
-	text += "Position: %s | OVR: %d\n" % [BlitzballPlayerData.Position.keys()[selected_agent.position], selected_agent.get_overall_rating()]
-	text += "Wage Requirement: [color=yellow]%d Gil[/color]\n\n" % selected_agent.salary
-	text += "SPD: %d | SHT: %d | PAS: %d | TCK: %d | CAT: %d\n\n" % [
-		selected_agent.speed, selected_agent.shooting, selected_agent.passing, selected_agent.tackling, selected_agent.reflexes
+	selected_agent = list_agents[index]
+	var text := "[b][font_size=22]%s[/font_size][/b]\n" % selected_agent.call("get_full_name")
+	text += "OVR: %d\n" % selected_agent.call("get_overall_rating")
+	text += "Wage Requirement: [color=yellow]%d Gil[/color]\n\n" % selected_agent.get("salary")
+	text += "SPD: %d | SHT: %d | PAS: %d | TCK: %d\n\n" % [
+		selected_agent.get("speed"), selected_agent.get("shooting"), selected_agent.get("passing"), selected_agent.get("tackling")
 	]
-	text += "[b]Techniques:[/b] %s" % (", ".join(selected_agent.learned_techniques) if not selected_agent.learned_techniques.is_empty() else "None")
 	details_label.text = text
 
-	sign_btn.disabled = (current_team.budget < selected_agent.salary)
+	sign_btn.disabled = (current_team.get("budget") < selected_agent.get("salary"))
 
 func _on_sign_agent() -> void:
 	if selected_agent and current_team:
-		if scouting_mgr.sign_free_agent(current_team, selected_agent):
+		if scouting_mgr.call("sign_free_agent", current_team, selected_agent):
 			_refresh_ui()
 
 func _on_back() -> void:
-	get_tree().change_scene_to_file("res://scenes/ui/SeasonDashboard.tscn")
+	get_tree().change_scene_to_file("res://season_dashboard.tscn")
